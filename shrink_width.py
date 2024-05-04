5ae0e7ce9a8f6e99b8c015efaa532639c4c8bd9f
Chenyang Yuan <yuanchenyang@gmail.com>
2024-05-04 14:13:09 -0700

    Added stretch

--- shrink_width.py
+++ shrink_width.py
@@ -11,6 +11,9 @@ def main():
     parser.add_argument('--new_img_ext',
                         help='New extension of image files',
                         default='_shrunk.jpg')
+    parser.add_argument('--stretch',
+                        help='use this flag to stretch instead of shink',
+                        action='store_true')
     parser.add_argument('--factor',
                         type=int,
                         help='rescale factor',
@@ -19,7 +22,8 @@ def main():
     for f in get_filenames(args):
         img = cv2.imread(f)
         H, W, _ = img.shape
-        img_new = cv2.resize(img, (W//args.factor, H),interpolation=cv2.INTER_AREA)
+        new_W = W * args.factor if args.stretch else W // args.factor
+        img_new = cv2.resize(img, (new_W, H),interpolation=cv2.INTER_AREA)
         cv2.imwrite(f.replace(args.ext, args.new_img_ext), img_new)
 
 if __name__=='__main__':
--- shrink_width_masks.py
+++ shrink_width_masks.py
@@ -10,6 +10,9 @@ def main():
     parser.add_argument('--new_mask_ext',
                         help='New extension of mask files',
                         default='_shrunk_cp_masks.png')
+    parser.add_argument('--stretch',
+                        help='use this flag to stretch instead of shink',
+                        action='store_true')
     parser.add_argument('--factor',
                         type=int,
                         help='rescale factor',
@@ -18,9 +21,9 @@ def main():
     for f in get_filenames(args):
         mask = open_masks(f)
         H, W = mask.shape
-        mask_new = cv2.resize(mask, (W//args.factor, H), interpolation=cv2.INTER_NEAREST)
+        new_W = W * args.factor if args.stretch else W // args.factor
+        mask_new = cv2.resize(mask, (new_W, H), interpolation=cv2.INTER_NEAREST)
         cv2.imwrite(f.replace(args.ext, args.new_mask_ext), mask_new)
-
 
 if __name__=='__main__':
     main()
